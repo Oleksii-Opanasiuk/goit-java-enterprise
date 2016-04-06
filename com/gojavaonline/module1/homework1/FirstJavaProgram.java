3e321ab1337838a3cd977547448285dce65dc645
Oleksii Opanasiuk <oleksii.opanasiuk@sixt.com>
2016-04-06 11:40:25 +0300

    first commit

---
+++ com/gojavaonline/module1/homework1/FirstJavaProgram.java
@@ -0,0 +1,12 @@
+package com.gojavaonline.module1.homework1;
+
+/**
+ * Created by alex on 25.12.15.
+ */
+public class FirstJavaProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
